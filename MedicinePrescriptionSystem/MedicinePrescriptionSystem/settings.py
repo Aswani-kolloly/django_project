8d2522dd481cdb083bec27a0c70514bec056d6ec
Aswani-kolloly <77399867+Aswani-kolloly@users.noreply.github.com>
2021-07-05 18:38:54 +0530

    Update settings.py

--- MedicinePrescriptionSystem/MedicinePrescriptionSystem/settings.py
+++ MedicinePrescriptionSystem/MedicinePrescriptionSystem/settings.py
@@ -23,9 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cc_swxg4c$0zk0(#d_8&di%h83i^n!*3nlgvn99f(negmrg&x='
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turnedn production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
